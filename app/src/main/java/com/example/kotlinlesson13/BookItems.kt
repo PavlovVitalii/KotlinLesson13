dc6f6b794ef805212f617ea872bfd1a2f5e35ddd
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2021-08-19 15:38:53 +0300

    Update adapter

--- app/src/main/java/com/example/kotlinlesson13/BookItems.kt
+++ app/src/main/java/com/example/kotlinlesson13/BookItems.kt
@@ -1,23 +1,18 @@
 package com.example.kotlinlesson13
 
-data class BookItems(
-    val name: String,
-    val autor: String,
-    val year: String,
-    val description: String
-)
+import com.example.kotlinlesson13.db.Book
 
 var bookListItems = listOf(
-    BookItems("Name: ", "Autor: ", "Year: ", "Description: "),
-    BookItems("Name: ", "Autor: ", "Year: ", "Description: "),
-    BookItems("Name: ", "Autor: ", "Year: ", "Description: "),
-    BookItems("Name: ", "Autor: ", "Year: ", "Description: "),
-    BookItems("Name: ", "Autor: ", "Year: ", "Description: "),
-    BookItems("Name: ", "Autor: ", "Year: ", "Description: "),
-    BookItems("Name: ", "Autor: ", "Year: ", "Description: "),
-    BookItems("Name: ", "Autor: ", "Year: ", "Description: "),
-    BookItems("Name: ", "Autor: ", "Year: ", "Description: "),
-    BookItems("Name: ", "Autor: ", "Year: ", "Description: "),
-    BookItems("Name: ", "Autor: ", "Year: ", "Description: "),
-    BookItems("Name: ", "Autor: ", "Year: ", "Description: "),
+    Book("Name: ", "Autor: ", "Year: ", "Description: "),
+    Book("Name: ", "Autor: ", "Year: ", "Description: "),
+    Book("Name: ", "Autor: ", "Year: ", "Description: "),
+    Book("Name: ", "Autor: ", "Year: ", "Description: "),
+    Book("Name: ", "Autor: ", "Year: ", "Description: "),
+    Book("Name: ", "Autor: ", "Year: ", "Description: "),
+    Book("Name: ", "Autor: ", "Year: ", "Description: "),
+    Book("Name: ", "Autor: ", "Year: ", "Description: "),
+    Book("Name: ", "Autor: ", "Year: ", "Description: "),
+    Book("Name: ", "Autor: ", "Year: ", "Description: "),
+    Book("Name: ", "Autor: ", "Year: ", "Description: "),
+    Book("Name: ", "Autor: ", "Year: ", "Description: "),
 )
